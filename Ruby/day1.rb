03c84b3dec3058c867b7c3510fdd8ca227d06479
Joshua DeLeon <joshua.deleon@metaltoad.com>
2016-08-12 00:11:54 -0700

    Add ruby files to Ruby directory

---
+++ Ruby/day1.rb
@@ -0,0 +1,13 @@
+# Print the string "Hello, World"
+puts "Hello, world"
+
+# Find the index of the word "Ruby"
+puts "Hello, Ruby".index('Ruby')
+
+# Print your name ten times
+(1..10).each { |index| 	puts "joshua deleon" } 
+
+# Print the string "This is sentence number {index}"
+(1..10).each do |index|
+	puts "This is sentence number #{index}"
+end
