8cce9513b008f391225f5d0c8c27ad89a0ed910b
Joshua DeLeon <josh@transtatic.com>
2016-09-03 00:39:05 -0700

    Adds Erlang day 1 problem 1

---
+++ Erlang/day1_p1.erl
@@ -0,0 +1,17 @@
+% Erlang Day 1 problem 1
+% Write a function that uses recursion to return the number of words in a string
+
+-module(day1_p1).
+-export([countWords/1]).
+
+countWords("") -> 0;
+countWords([32|[]]) -> 0;
+countWords([A|[]]) -> 1;
+countWords([B, 32 | C]) -> 
+	if B == 32 	-> countWords(C); 
+		true 		-> 1 + countWords(C)
+	end;
+countWords([D|E]) -> countWords(E).
+
+% much much simpler to use built-in function string:words
+% very unsure how much of the language to use for this problem
